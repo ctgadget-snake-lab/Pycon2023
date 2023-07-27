1d1802e307cec4b8f7b57dad5e7156652f345380
DEBRA-LAPTOP\Debra Graves <chaance.graves@ctginnovations.io>
2023-07-27 16:16:44 -0500

    added sphinxy project tutorial to Pycon2023 folder.

---
+++ sphinxy2/docs/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Sphinxy'
+copyright = '2023, ctgadget'
+author = 'ctgadget'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
+    "sphinx_multiversion",
+]
+
+autoclass_content = 'both'
+templates_path = ['_templates']
+html_sidebars = {
+    "**": [
+        "sidebar/brand.html",
+        "sidebar/search.html",
+        "sidebar/scroll-start.html",
+        "sidebar/navigation.html",
+        "sidebar/versions.html",
+        "sidebar/scroll-end.html",
+    ],
+}
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+html_favicon = "_static/favicon.ico"
---
+++ sphinxy2/src/sphinxy/sphinx.py
@@ -0,0 +1,69 @@
+from sphinxy.riddle import Riddle
+
+
+class IncorrectAnswer(Exception):
+    ...
+
+
+class Sphinx:
+    def __init__(self, name: str):
+        self._name = name
+        self._riddle = Riddle(
+            question=(
+                "What goes on four legs in the morning, two legs at noon, "
+                "and three legs in the evening?"
+            ),
+            answer="man",
+        )
+
+    def introduce(self) -> str:
+        return (
+            f"Greetings, mortals. I am {self._name}. I have guarded the city of Thebes"
+            "for centuries and posed riddles to those who dared to approach me."
+        )
+
+    def update_riddle(self, riddle: Riddle) -> str:
+        """
+        Update the riddle for the user to answer.
+
+        Args:
+            riddle (str): The riddle given to the user.
+
+        Returns:
+            str: The riddle
+        """
+        self._riddle = riddle
+        return "I have updated my riddle. Are you ready to solve it?"
+
+    def pose_riddle(self, include_hint: bool = False) -> tuple[str, str | None]:
+        hint = (
+            f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
+            if include_hint
+            else None
+        )
+        return (self._riddle.question, hint)
+
+    def check_riddle_answer(self, answer: str, return_hint: bool = False) -> str:
+        """
+        Evaluates the given answer to the riddle.
+
+        Args:
+            answer (str): The given answer to the riddle.
+            return_hint (bool, optional): Controls whether a hint for the riddle should
+                be returned. Defaults to False.
+
+        Raises:
+            IncorrectAnswer: Exception for incorrect answer.
+
+        Returns:
+            str: The result of the evaluation of the answer.
+        """
+        if self._riddle.check_answer(answer):
+            return "Your answer was correct. You may pass."
+        elif return_hint:
+            return (
+                "Your answer was wrong. Hint: The answer starts with the letter "
+                f"'{self._riddle.get_hint()}'."
+            )
+        else:
+            raise IncorrectAnswer("Your answer was wrong. You shall not pass.")
